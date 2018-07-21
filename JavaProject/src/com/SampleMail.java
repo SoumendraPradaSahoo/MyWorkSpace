b945f7db9f5778c2e25eff306aeb6a5a1216ee22
SoumendraPrasadSahoo <somu123_pdp@yahoo.co.in>
2018-07-21 15:45:37 +0530

    4th Commit

--- JavaProject/src/com/SampleMail.java
+++ JavaProject/src/com/SampleMail.java
@@ -1,27 +1,34 @@
 package com;
 import java.util.Properties;
-
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class SampleMail {
 
 	public static void main(String[] args) {
 
 		final String username = "XXXXX@gmail.com";
-		final String password = "XXXXX";
-		final String to = "XXXXXXX@XXXXX.co.in";
+		final String password = "XXXXXX";
+		final String to = "XXXXXXX.co.in";
+		final String smtpServer = "smtp.gmail.com";
 
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		props.put("mail.smtp.host", smtpServer);
+		props.put("mail.smtp.ssl.trust", smtpServer);
 //		props.put("mail.smtp.host", "smtp.mail.yahoo.com");
 //		props.put("mail.smtp.ssl.trust", "smtp.mail.yahoo.com");
 		props.put("mail.smtp.port", "587");
@@ -44,7 +51,24 @@ public class SampleMail {
 //					InternetAddress.parse("email@mail.com"));
 			message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			message.setSubject("Testing Subject");
-			message.setText("Dear Mr XXXXXX," + "\n\n This is a test e-mail, please ignore!");
+//			message.setText("Dear Mr XXX," + "\n\n This is a test e-mail, please ignore!");
+//			message.setFileName("\\Desktop\\sample.pdf");
+			
+			BodyPart messageBodyPart = new MimeBodyPart();
+			messageBodyPart.setText("Dear Mr XXXX," + "\n\n This is a test e-mail, please ignore!");
+			
+			Multipart multiPartMesssage = new MimeMultipart();
+			multiPartMesssage.addBodyPart(messageBodyPart);
+			
+			messageBodyPart = new MimeBodyPart();
+			DataSource source = new FileDataSource("\\Desktop\\sample.pdf");	
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName("sample File.PDF");
+			multiPartMesssage.addBodyPart(messageBodyPart);
+			
+			message.setContent(multiPartMesssage);
+			
+			
 			System.out.println("Message composed");
 			//Transport transport = session.getTransport("smtp");
 			Transport.send(message);
