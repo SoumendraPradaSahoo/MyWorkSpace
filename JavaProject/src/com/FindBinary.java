8790c642e437e521079fc1272ada61c1d69e1f31
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2018-08-23 15:34:31 +0530

    New Class for Binary No

---
+++ JavaProject/src/com/FindBinary.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.util.Scanner;
+
+public class FindBinary {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int temp = sc.nextInt();
+		int test=temp;
+		int rem=0;
+		String out = "";
+		if (temp==0)
+				out="0";
+		while(temp>=1)
+		{
+		rem=temp%2;
+		out= rem + out;
+		temp=temp/2;
+		}
+		System.out.println("Binary of " + test + " is " + out);
+		sc.close();
+	}
+
+}
