0c510d5b38b370e1426bcaa14e5d7b1a1adab0c9
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2018-09-06 14:17:08 +0530

    Updated function to set max for 1st bene

--- JavaProject/src/com/BenePercentage.java
+++ JavaProject/src/com/BenePercentage.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class BenePercentage {
 
 	public static void main(String[] args) {
-		int noBene = 7;
+		int noBene = 6;
 		Map<String, String> BenePercentage = new HashMap<String, String>();
 		int totalpercentage=100;
 		int intPercentage;
 		for(int i=1;i<=noBene;i++) {
 			intPercentage = totalpercentage/(noBene-i+1);
-			BenePercentage.put("A_InterestPercent_BEN" + i, Integer.toString(intPercentage));
+			BenePercentage.put("A_InterestPercent_BEN" + (noBene - i + 1), Integer.toString(intPercentage));
 			totalpercentage = totalpercentage - intPercentage;
 			}
 		for(Map.Entry<String, String> entry : BenePercentage.entrySet())
