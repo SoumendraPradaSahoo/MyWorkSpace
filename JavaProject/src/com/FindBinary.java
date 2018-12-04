1884db128ba5007a37cd0457d71f704b4b7b8da0
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2018-12-04 15:50:15 +0530

    New commit on 12/04

--- JavaProject/src/com/FindBinary.java
+++ JavaProject/src/com/FindBinary.java
@@ -9,14 +9,16 @@ public class FindBinary {
 		int temp = sc.nextInt();
 		int test=temp;
 		int rem=0;
-		String out = "";
+		int out=0;
+		int i=0;
 		if (temp==0)
-			out="0";
+			out=0;
 		while(temp>=1)
 		{
 			rem=temp%2;
-			out= rem + out;
+			out= rem*((int)Math.pow(10, i++)) + out;
 			temp=temp/2;
+			System.out.println(out + " " + i);
 		}
 		System.out.println("Binary of " + test + " is " + out);
 		sc.close();
