4307c13bb88fcd61704a3d556d3e7e07fb035db2
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2019-01-09 18:16:43 +0530

    DataBase Testing java updated

--- JavaProject/src/com/DatabaseTesting.java
+++ JavaProject/src/com/DatabaseTesting.java
@@ -37,18 +37,28 @@ public class DatabaseTesting {
             connection = DriverManager.getConnection(dbURL); 
 
             // Step 2.B: Creating JDBC Statement 
-            statement = connection.createStatement();
+            //statement = connection.createStatement(); //without scrollable resultset
+            statement = connection.createStatement( ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);      //scrollable resultset      
 
             // Step 2.C: Executing SQL & retrieve data into ResultSet
             resultSet = statement.executeQuery("SELECT * FROM NBA_BankAccounts");
+            
+            //moving the cursor to last record
+            resultSet.last();
+            
+            System.out.println("Total no of records :" + resultSet.getRow());
+            
+            //move the resultset to before 1st row
+            resultSet.beforeFirst();
 
             System.out.println("Bank Account\tTransalation");
             System.out.println("==============\t===============================");
 
             // processing returned data and printing into console
             while(resultSet.next()) {
-                System.out.println(resultSet.getString(5) + "\t\t" + 
-                        resultSet.getString(6));
+                System.out.println(resultSet.getString("BANK_ACCT_NUM") + "\t\t" + 
+                        resultSet.getString("TRANSLATION"));
             }
         }
         catch(SQLException sqlex){
