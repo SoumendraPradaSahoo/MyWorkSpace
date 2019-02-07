2c6af17e931bc6fad88e1cbfc7d65569b2147b25
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2019-02-07 13:27:13 +0530

    Commited Changes

--- JavaProject/src/com/CountChar.java
+++ JavaProject/src/com/CountChar.java
@@ -7,27 +7,45 @@ public class CountChar
     /* Fills count array with frequency of characters */
     static void fillCharCounts(String str, int[] count) 
     { 
-       for (int i = 0; i < str.length();  i++) 
+       for (int i = 0; i < str.length();  i++) {
+    	  //int a = Character.getNumericValue(str.charAt(i));
+    	 // if (str.contains(String.valueOf(str.charAt(i))))
           count[str.charAt(i)]++; 
-    } 
+    } }
        
     /* Print duplicates present in the passed string */
     static void printDups(String str) 
     { 
+    	
       // Create an array of size 256 and fill count of every character in it 
       int count[] = new int[NO_OF_CHARS]; 
+      int max_Count=0;
+      String charwithMaxCount="";
       fillCharCounts(str, count); 
       
-      for (int i = 0; i < NO_OF_CHARS; i++) 
-       if(count[i] > 0) 
+      
+      for (int i = 0; i < NO_OF_CHARS; i++) {
+       if(count[i] > 0) {
             System.out.printf("%c,  count = %d \n", i,  count[i]); 
+            if (count[i]>max_Count) {
+            	max_Count=count[i];
+            	charwithMaxCount= String.valueOf((char)i);
+            }else if(count[i]==max_Count) {
+            	charwithMaxCount = charwithMaxCount + ", " + String.valueOf((char)i);
+            }
+       }
+      }
+      
+      System.out.println("Max occurance of character(s) : '" + charwithMaxCount + "' is " + max_Count);
        
     } 
        
     // Driver Method 
     public static void main(String[] args) 
     { 
-        String str = "test string"; 
+    	System.out.println("Start Time: " + System.currentTimeMillis());
+        String str = "Spring Test"; 
         printDups(str); 
+        System.out.println("End Time: " + System.currentTimeMillis());
     } 
 } 
