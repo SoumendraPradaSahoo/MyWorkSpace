72da4a9a43c01e1deb869f8a4e88873370d49ad0
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2019-02-22 15:43:22 +0530

    Changed For Loop

--- JavaProject/src/com/FindxslFile.java
+++ JavaProject/src/com/FindxslFile.java
@@ -14,12 +14,12 @@ public class FindxslFile {
 	
 	public void getFiles(File fileName) {
 		File[] filesinDir = fileName.listFiles();
-		for(int i=0;i<filesinDir.length;i++) {
-			if(filesinDir[i].isDirectory()) {
-				getFiles(filesinDir[i]);	
+		for(File fl: filesinDir) {
+			if(fl.isDirectory()) {
+				getFiles(fl);	
 			}
-			else if(filesinDir[i].getName().contains(".xlsx"))
-			System.out.println(filesinDir[i].getAbsolutePath());
+			else if(fl.getName().contains(".xlsx"))
+			System.out.println(fl.getAbsolutePath());
 		}
 	}
 
