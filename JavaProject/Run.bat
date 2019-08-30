643841536d4582e0517a5578ada2d2780249ba39
Soumendra Prasad Sahoo <somu123_pdp@yahoo.co.in>
2019-08-30 18:17:12 +0530

    Updated bat file to include path

--- JavaProject/Run.bat
+++ JavaProject/Run.bat
@@ -1,1 +1,2 @@
-java -cp D:\TestNG\*;D:\MyWorkSpace\JavaProject\bin org.testng.TestNG TestNgExecution.xml
+cd C:\Users\ssahoo43\D_Drive\MyWorkSpace\JavaProject
+java -cp C:\Users\ssahoo43\D_Drive\TestNG\*;C:\Users\ssahoo43\D_Drive\MyWorkSpace\JavaProject\bin org.testng.TestNG TestNgExecution.xml
